8d6473ef5bec1fd9cea5fc5e7e2a2465f719f509
Rachel-code-coder <vy.ngothuyy@gmail.com>
2026-02-20 21:52:25 -0500

    update student info

--- src/App.js
+++ src/App.js
@@ -4,23 +4,26 @@ import StudentForm from "./components/StudentForm";
 import StudentList from "./components/StudentList";
 
 const App = () => {
-  const [students, setStudents] = useState([]);
+    const [students, setStudents] = useState([]);
 
-  const addStudent = (student) => {
-    setStudents([...students, student]);
-  };
+    const addStudent = (student) => {
+        setStudents([...students, student]);
+    };
 
-  return (
-    <div className="App">
-      <h1>Studen ID: Put Your Student ID</h1>
-      <h1>Studen ID: Put Your Student Name</h1>
-      <h1>Student Information System</h1>
-      <h5>Powered By: moxDroid Labs Inc. || DevOps</h5>
-      <StudentForm addStudent={addStudent} />
-      Developerd By: Pritesh Patel
-      <StudentList students={students} />
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Student ID: 101511264</h1>
+            <h1>Student Name: Vy Ngo</h1>
+            <h1>Student Information System</h1>
+            <h5>Powered By: DevOps</h5>
+
+            <StudentForm addStudent={addStudent} />
+
+            <p>Developed By: Vy Ngo</p>
+
+            <StudentList students={students} />
+        </div>
+    );
 };
 
 export default App;
